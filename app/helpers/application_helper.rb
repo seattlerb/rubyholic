16e3f4997713a71da4bdfe5974c77e346c434890
Ryan Davis <ryand@zenspider.com>
2005-11-12 01:07:35 -0800

    Added versioning. Improved TODO list. Added header and footer code, but not much there. Made a stab at some more CSS, I suck at it tho.
    [git-p4: depot-paths = "//src/rubyholic/dev/": change = 2226]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,18 @@
 module ApplicationHelper
+
+  RUBYHOLIC_VERSION = "1.0 beta" unless defined? RUBYHOLIC_VERSION
+
   def form_field(type, model, column, legend, options={})
     label = %(<th><label for="#{model}_#{column}">#{legend}</label></th>)
     field = %(<td>#{send "#{type}_field", model, column, options}</td>)
     return ['<tr>', label, field, '</tr>'].join("\n")
+  end
+  
+  def header
+    %(<span class="header"><a href="http://www.rubyholic.com/">[Home]</a></span>)
+  end
+  
+  def footer
+    %(<p class="footer"><a href="http://www.rubyholic.com/">Rubyholic.com</a>, version #{RUBYHOLIC_VERSION}</p>)
   end
 end
